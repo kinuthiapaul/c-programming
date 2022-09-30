5dca2be935cfbeca88111f2fed5d8bb621b7b6b8
kinuthiapaul <114053335+kinuthiapaul@users.noreply.github.com>
2022-09-30 15:20:54 +0300

    Create paull.cpp

---
+++ paull.cpp
@@ -0,0 +1,14 @@
+// programm to print even numbers 
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int i;
+    for(i=0;i<=100; i+=2)
+    {
+        cout<<i<<"  ";
+    }
+
+
+    return 0;
+}
